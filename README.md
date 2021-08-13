ad0c9b56bedb39c7541d75eae34527900c9521c3
crackerFactory64 <lee.mander@gmail.com>
2021-08-13 15:49:00 +0100

    functionality complete

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# tip-calculator-app-main
- Frontend Mentor project
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./desktop.png)
+![](./mobile.png)
+
+### Links
+
+- Solution URL: [https://github.com/crackerFactory64/tip-calculator-app-main](https://github.com/crackerFactory64/tip-calculator-app-main)
+- Live Site URL: [https://crackerfactory64.github.io/tip-calculator-app-main/](https://crackerfactory64.github.io/tip-calculator-app-main/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
--- main.js
+++ main.js
@@ -8,61 +8,7 @@ let buttons = document.getElementsByClassName("form__button--percentage");
 
 let inputs = document.getElementsByClassName("form__input");
 
-for (let i = 0; i < inputs.length; i++) {
-  inputs[i].onfocus = () => {
-    inputs[i].parentNode.style.borderColor = "#5BA89E";
-  };
-  inputs[i].onblur = () => {
-    if (i == 1) {
-      inputs[1].onchange = () => {
-        for (let j = 0; j < buttons.length; j++) {
-          buttons[j].classList.remove("form__button--percentage--selected");
-        }
-        if (inputs[1].value > 0) {
-          tip = inputs[1].value / 100;
-          inputs[1].parentNode.style.borderColor = "transparent";
-          document.getElementById("error" + (i + 1)).style.visibility =
-            "hidden";
-        } else {
-          inputs[1].parentNode.style.borderColor = "#FF5B33";
-          document.getElementById("error" + (i + 1)).style.visibility =
-            "visible";
-        }
-      };
-      inputs[1].parentNode.style.borderColor = "transparent";
-    } else {
-      if (inputs[i].value <= 0) {
-        inputs[i].parentNode.style.borderColor = "#FF5B33";
-        document.getElementById("error" + (i + 1)).style.visibility = "visible";
-      } else {
-        inputs[i].parentNode.style.borderColor = "transparent";
-        document.getElementById("error" + (i + 1)).style.visibility = "hidden";
-      }
-    }
-  };
-}
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].onclick = () => {
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove("form__button--percentage--selected");
-    }
-    buttons[i].classList.add("form__button--percentage--selected");
-    inputs[1].parentNode.style.borderColor = "transparent";
-    document.getElementById("error2").style.visibility = "hidden";
-    if (i != 5) {
-      tip = buttons[i].id / 100;
-    }
-    tipPerPerson = (bill / people) * tip;
-    if (bill != 0 && people != 0) {
-      document.getElementById("tip").innerHTML = "$" + tipPerPerson.toFixed(2);
-      document.getElementById("total").innerHTML =
-        "$" + (totalPerPerson + tipPerPerson).toFixed(2);
-    }
-  };
-}
-
-document.onkeyup = () => {
+const update = () => {
   bill = document.getElementById("bill").value;
   people = document.getElementById("people").value;
   tipPerPerson = (bill / people) * tip;
@@ -73,6 +19,64 @@ document.onkeyup = () => {
       "$" + (totalPerPerson + tipPerPerson).toFixed(2);
   }
 };
+
+for (let i = 0; i < inputs.length; i++) {
+  inputs[i].onfocus = () => {
+    inputs[i].parentNode.style.borderColor = "#5BA89E";
+  };
+  if (i == 1) {
+    inputs[1].onchange = () => {
+      for (let j = 0; j < buttons.length; j++) {
+        buttons[j].classList.remove("form__button--percentage--selected");
+      }
+      if (inputs[1].value > 0) {
+        tip = inputs[1].value / 100;
+        inputs[1].parentNode.style.borderColor = "transparent";
+        document.getElementById("error" + (i + 1)).style.visibility = "hidden";
+      } else {
+        inputs[1].parentNode.style.borderColor = "#FF5B33";
+        document.getElementById("error" + (i + 1)).style.visibility = "visible";
+      }
+    };
+    inputs[1].onblur = () => {
+      inputs[1].parentNode.style.borderColor = "transparent";
+    };
+  } else {
+    inputs[i].onblur = () => {
+      if (inputs[i].value <= 0) {
+        inputs[i].parentNode.style.borderColor = "#FF5B33";
+        document.getElementById("error" + (i + 1)).style.visibility = "visible";
+      } else {
+        inputs[i].parentNode.style.borderColor = "transparent";
+        document.getElementById("error" + (i + 1)).style.visibility = "hidden";
+      }
+    };
+  }
+}
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = () => {
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove("form__button--percentage--selected");
+    }
+    buttons[i].classList.add("form__button--percentage--selected");
+
+    document.getElementById("error2").style.visibility = "hidden";
+    if (i != 5) {
+      inputs[1].parentNode.style.borderColor = "transparent";
+      tip = buttons[i].id / 100;
+      inputs[1].value = "";
+    }
+    if (bill != 0 && people != 0) {
+      document.getElementById("tip").innerHTML = "$" + tipPerPerson.toFixed(2);
+      document.getElementById("total").innerHTML =
+        "$" + (totalPerPerson + tipPerPerson).toFixed(2);
+    }
+  };
+}
+
+document.onclick = update;
+document.onchange = update;
 
 document.getElementsByClassName("form__button--reset").onclick = () => {
   location.reload();
